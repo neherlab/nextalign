952856b8525fa69138fa45c36d433d0350a90559
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-11-17 16:00:53 +0100

    feat: add seqan2-based alignment code
    https://github.com/iosonofabio/seqanpy

---
+++ packages/nextalign/src/seqan2.cpp
@@ -0,0 +1,172 @@
+/**
+ * Adopted with modifications from https://github.com/iosonofabio/seqanpy
+ * Thanks Fabio Zanini @iosonofabio
+ */
+#include <seqan/align.h>
+
+#include "seqan2.h"
+
+using namespace seqan;
+
+
+// Functions
+int align_global(std::string seq1, std::string seq2, std::string* ali1, std::string* ali2, int band,
+                 int score_match, int score_mismatch, int score_gapext, int score_gapopen) {
+
+	typedef String<char> TSequence;                 // sequence type
+	typedef Align<TSequence,ArrayGaps> TAlign;      // align type
+	typedef Row<TAlign>::Type TRow;                 // gapped sequence type
+	std::stringstream rowstream;
+	int score;
+
+	// Build alignment object
+	TAlign ali;
+        resize(rows(ali), 2);
+	assignSource(row(ali, 0), seq1);
+	assignSource(row(ali, 1), seq2);
+
+	// Align
+	if(band >= 0)
+		score = globalAlignment(ali,
+					Score<int,Simple>(score_match, score_mismatch,
+                                                          score_gapext, score_gapopen),
+					-band, band);
+	else
+		score = globalAlignment(ali,
+					Score<int,Simple>(score_match, score_mismatch,
+                                                          score_gapext, score_gapopen));
+
+	// Set the output
+	TRow row1 = row(ali,0);
+	rowstream.str("");
+	rowstream << row1;
+	(*ali1) = rowstream.str();
+
+	TRow row2 = row(ali,1);
+	rowstream.str("");
+	rowstream << row2;
+	(*ali2) = rowstream.str();
+
+	return score;
+}
+
+
+int align_overlap(std::string seq1, std::string seq2, std::string* ali1, std::string* ali2, int band,
+                 int score_match, int score_mismatch, int score_gapext, int score_gapopen, int cut_flanks) {
+
+	typedef String<char> TSequence;                 // sequence type
+	typedef Align<TSequence,ArrayGaps> TAlign;      // align type
+	typedef Row<TAlign>::Type TRow;                 // gapped sequence type
+	std::stringstream rowstream;
+	int score;
+
+	// Build alignment object
+	TAlign ali;
+        resize(rows(ali), 2);
+	assignSource(row(ali, 0), seq1);
+	assignSource(row(ali, 1), seq2);
+
+	// Align
+	if(band >= 0)
+		score = globalAlignment(ali,
+					Score<int,Simple>(score_match, score_mismatch,
+                                                          score_gapext, score_gapopen),
+					AlignConfig<true, false, false, true>(),
+					-band, band);
+	else
+		score = globalAlignment(ali,
+					Score<int,Simple>(score_match, score_mismatch,
+                                                          score_gapext, score_gapopen),
+					AlignConfig<true, false, false, true>());
+
+	// Set the output
+	TRow row1 = row(ali,0);
+	rowstream.str("");
+	rowstream << row1;
+	(*ali1) = rowstream.str();
+
+	TRow row2 = row(ali,1);
+	rowstream.str("");
+	rowstream << row2;
+	(*ali2) = rowstream.str();
+
+	return score;
+}
+
+
+int align_ladder(std::string seq1, std::string seq2, std::string* ali1, std::string* ali2, int band,
+                 int score_match, int score_mismatch, int score_gapext, int score_gapopen) {
+
+	typedef String<char> TSequence;                 // sequence type
+	typedef Align<TSequence,ArrayGaps> TAlign;      // align type
+	typedef Row<TAlign>::Type TRow;                 // gapped sequence type
+	std::stringstream rowstream;
+	int score;
+
+	// Build alignment object
+	TAlign ali;
+        resize(rows(ali), 2);
+	assignSource(row(ali, 0), seq1);
+	assignSource(row(ali, 1), seq2);
+
+	// Align
+	if(band >= 0)
+		score = globalAlignment(ali,
+					Score<int,Simple>(score_match, score_mismatch,
+                                                          score_gapext, score_gapopen),
+					AlignConfig<true, false, true, false>(),
+					-band, band);
+	else
+		score = globalAlignment(ali,
+					Score<int,Simple>(score_match, score_mismatch,
+                                                          score_gapext, score_gapopen),
+					AlignConfig<true, false, true, false>());
+
+	// Set the output
+	TRow row1 = row(ali,0);
+	rowstream.str("");
+	rowstream << row1;
+	(*ali1) = rowstream.str();
+
+	TRow row2 = row(ali,1);
+	rowstream.str("");
+	rowstream << row2;
+	(*ali2) = rowstream.str();
+
+	return score;
+}
+
+
+int align_local(std::string seq1, std::string seq2, std::string* ali1, std::string* ali2,
+                 int score_match, int score_mismatch, int score_gapext, int score_gapopen) {
+
+	typedef String<char> TSequence;                 // sequence type
+	typedef Align<TSequence,ArrayGaps> TAlign;      // align type
+	typedef Row<TAlign>::Type TRow;                 // gapped sequence type
+	std::stringstream rowstream;
+	int score;
+
+	// Build alignment object
+	TAlign ali;
+        resize(rows(ali), 2);
+	assignSource(row(ali, 0), seq1);
+	assignSource(row(ali, 1), seq2);
+
+        // Align (best alignment only)
+        score = localAlignment(ali,
+                               Score<int,Simple>(score_match, score_mismatch,
+                                                 score_gapext, score_gapopen));
+
+	// Set the output
+	TRow row1 = row(ali,0);
+	rowstream.str("");
+	rowstream << row1;
+	(*ali1) = rowstream.str();
+
+	TRow row2 = row(ali,1);
+	rowstream.str("");
+	rowstream << row2;
+	(*ali2) = rowstream.str();
+
+        return score;
+}
---
+++ packages/nextalign/src/seqan2.h
@@ -0,0 +1,41 @@
+/**
+ * Adopted with modifications from https://github.com/iosonofabio/seqanpy
+ * Thanks Fabio Zanini @iosonofabio
+ */
+// vim:
+/*
+ *  Created on:	26/01/2014
+ *  Author:	Fabio Zanini
+ *  Contents:	Main header for the python wrapper of the SeqAn library.
+ */
+// Includes
+#include <string>
+#include <iostream>
+#include <sstream>
+
+// Functions
+int nothing();
+
+int align_global(std::string seq1, std::string seq2,
+                 std::string* aliout1, std::string* aliout2, int band=100,
+                 int score_match=3, int score_mismatch=-3,
+                 int score_gapext=-1, int score_gapopen=-5);
+
+
+int align_overlap(std::string seq1, std::string seq2,
+                  std::string* aliout1, std::string* aliout2, int band=-1,
+                  int score_match=3, int score_mismatch=-3,
+                  int score_gapext=-1, int score_gapopen=-5,
+                  int cut_flanks=0);
+
+
+int align_ladder(std::string seq1, std::string seq2,
+                 std::string* aliout1, std::string* aliout2, int band=-1,
+                 int score_match=3, int score_mismatch=-3,
+                 int score_gapext=-1, int score_gapopen=-5);
+
+
+int align_local(std::string seq1, std::string seq2,
+                std::string* aliout1, std::string* aliout2,
+                int score_match=3, int score_mismatch=-3,
+                int score_gapext=-1, int score_gapopen=-5);
