bb90bf01298b8a9c6aff7104080d99504f38b9a6
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-11-17 17:36:30 +0100

    feat: use seqan2 implementation in alignPairwise()

--- packages/nextalign/src/alignPairwise.cpp
+++ packages/nextalign/src/alignPairwise.cpp
@@ -2,13 +2,23 @@
 
 #include <string>
 
+#include "helpers.h"
+#include "seqan2.h"
 
 Alignment alignPairwise(const std::string& query, const std::string& ref, int minimalLength) {
-  (void) minimalLength;// unused
 
-  const auto alignedQuery = query;
-  const auto alignedRef = ref;
-  const auto alignmentScore = 0;
+  NA_UNUSED(minimalLength);
 
-  return {.query = alignedQuery, .ref = alignedRef, .alignmentScore = alignmentScore};
+  const AlignOptionsOverlap options = {
+    .band = 5,
+    .score_match = 1,
+    .score_mismatch = -1,
+    .score_gapext = 1,
+    .score_gapopen = -1,
+    .cut_flanks = 0,
+  };
+
+  const AlignmentResult& result = align_overlap(query, ref, options);
+
+  return {.query = result.seq1, .ref = result.seq2, .alignmentScore = result.score};
 }
