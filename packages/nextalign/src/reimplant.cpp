338e0fd4bcfd2b45e69fccc1f5537d2573337cd2
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-12-15 17:05:48 +0100

    feat: reimplant gaps

--- packages/nextalign/src/reimplant.cpp
+++ packages/nextalign/src/reimplant.cpp
@@ -17,28 +17,25 @@ Alignment reimplant(Alignment& alignmentImproved, const CodonAlignmentResult& co
   auto& query = alignmentImproved.query;
   const auto queryLength = safe_cast<int>(query.size());
 
-
   const auto& refPeptide = std::string_view{codonAlignmentResult.refPeptide};
-  const auto refPeptideLength = safe_cast<int>(refPeptide.size());
 
   const auto& queryPeptide = std::string_view{codonAlignmentResult.queryPeptide};
   const auto queryPeptideLength = safe_cast<int>(queryPeptide.size());
 
-
   for (int i_aa = 0; i_aa < queryPeptideLength; ++i_aa) {
-    invariant_less(i_aa, queryPeptideLength);
-    const auto& aa_query = queryPeptide[i_aa];
-
-    invariant_less(i_aa, refPeptideLength);
-    const auto& aa_ref = refPeptide[i_aa];
-
     const int i_nuc = gene.start + i_aa * 3;
-    invariant_less(i_nuc, queryLength);
-    const auto hint = std::string_view{query}.substr(i_nuc, 3);
 
-    const auto codon = encode(aa_query, hint);
+    invariant_less(i_aa, refPeptide.size());
+    invariant_less(i_aa, queryPeptide.size());
+    invariant_less(i_nuc, query.size());
+
+    const auto& aa_query = queryPeptide[i_aa];
+    if (aa_query == AMINOACID_GAP) {
+      query[i_nuc + 0] = '-';
+      query[i_nuc + 1] = '-';
+      query[i_nuc + 2] = '-';
+    }
   }
-
 
   postcondition_equal(query.size(), queryLength);// query length should not change
   return alignmentImproved;
