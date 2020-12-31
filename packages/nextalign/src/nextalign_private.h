f0abe1074511fe8b23522589d950d595064b9f62
ivan-aksamentov <ivan.aksamentov@gmail.com>
2020-12-31 02:20:46 +0100

    chore: remove unused code

--- packages/nextalign/src/nextalign_private.h
+++ packages/nextalign/src/nextalign_private.h
@@ -8,12 +8,6 @@ template<typename Letter>
 using SequenceSpan = gsl::basic_string_span<Letter, gsl::dynamic_extent>;
 
 
-struct CodonAlignmentResult {
-  AminoacidSequence queryPeptide;
-  AminoacidSequence refPeptide;
-  int alignmentScore;
-};
-
 struct PeptideInternal {
   std::string name;
   AminoacidSequence seq;
